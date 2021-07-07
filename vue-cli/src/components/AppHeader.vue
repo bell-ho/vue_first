9271804e5827664fa8834e203cb302fc892d8b4e
bell-ho <jj0101065@gmail.com>
2021-07-07 23:54:32 +0900

    Vue cli

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>{{ dataprops }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+<script>
+export default {
+  props: ['dataprops'],
+  methods: {
+    sendEvent: function () {
+      this.$emit('renew');  //renew 이벤트 방출
+    }
+  }
+}
+</script>
+<style>
+
+</style>
