a515c5828e2bbf61c96e7f5708dfa19d9e5e776b
bell-ho <jj0101065@gmail.com>
2021-07-18 14:07:36 +0900

    Vue cors 203

--- vue-project/blob/master/vue.config.js
+++ vue-project/blob/master/vue.config.js
@@ -1,4 +1,15 @@
+const target = 'http://127.0.0.1:3000'; //proxy 요청을 보낼 서버 주소
 module.exports = {
+    devServer: {
+      port:8080,
+      proxy:{
+          //proxy 요청을 보낼 api 시작 부분
+          '^/api':{
+              target,
+              changeOrigin: true
+          }
+      }
+    },
     chainWebpack: config => {
         config.plugin.delete('prefetch'); // prefetch 삭제
     }
